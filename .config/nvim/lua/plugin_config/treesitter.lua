712d7014503d2c2338047b8892218b495811b595
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-01-10 19:22:42 -0500

    thing

--- .config/nvim/lua/plugin_config/treesitter.lua
+++ .config/nvim/lua/plugin_config/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "rust", "vim", "bash" },
+  ensure_installed = { "bash" },
   sync_install = false,
   auto_install = true,
   highlight = {
