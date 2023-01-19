030fc6aacd1858c4b2c9a31a86a1ad6c6dfb8d12
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-01-19 00:49:27 -0500

    a thing

--- .config/nvim/lua/plugin_config/debug.lua
+++ .config/nvim/lua/plugin_config/debug.lua
@@ -2,16 +2,18 @@ vim.api.nvim_exec([[
 " We're going to define single-letter keymaps, so don't try to define them
 " in the terminal window.  The debugger CLI should continue accepting text commands.
 function! NvimGdbNoTKeymaps()
-  tnoremap <silent> <buffer> <esc> <c-\><c-n>
+    tnoremap <silent> <buffer> <esc> <c-\><c-n>
 endfunction
 
 let g:nvimgdb_config_override = {
-  \ 'key_next': 'n',
-  \ 'key_step': 's',
-  \ 'key_finish': 'f',
-  \ 'key_continue': 'c',
-  \ 'key_until': 'u',
-  \ 'key_breakpoint': 'b',
-  \ 'set_tkeymaps': "NvimGdbNoTKeymaps",
-  \ }
+    \ 'key_next': 'n',
+    \ 'key_step': 's',
+    \ 'key_finish': 'f',
+    \ 'key_continue': 'c',
+    \ 'key_until': 'u',
+    \ 'key_breakpoint': 'b',
+    \ 'set_tkeymaps': "NvimGdbNoTKeymaps",
+    \ }
+
+command! -nargs=* Bashdb GdbStartBashDB bashdb %p <f-args>
 ]], false)
