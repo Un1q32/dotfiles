1170f6d552cdbfe025a7b808c6edcd7cb751cdc3
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-03-04 00:04:50 -0500

    remove bashdb keymap

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -2,4 +2,3 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', { silent = true })
-vim.keymap.set('n', '<leader>d', ':GdbStartBashDB bashdb %p<CR>', { silent = true })
