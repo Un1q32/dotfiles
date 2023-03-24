851291f79db41e735c8bd56424008ebd1cc1e10e
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-03-24 14:40:11 -0400

    better powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -12,10 +12,21 @@ Import-Module PSReadLine
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadlineOption -EditMode vi
 
+function promptpath {
+    $path = Get-Location
+    $path = $path.Path
+    $path = $path.Replace($HOME, '~')
+    $path = $path.Replace('\', '/')
+    return $path
+}
+
 function prompt {
     Write-Host "$($env:username)@$(hostname) " -ForegroundColor Green -NoNewLine
-    Write-Host "$(Get-Location) `$" -ForegroundColor Blue -NoNewLine
+    Write-Host "$(promptpath) `$" -ForegroundColor Blue -NoNewLine
     return ' '
 }
 
 $env:CARGO_REGISTRIES_CRATES_IO_PROTOCOL = "sparse"
+$env:PF_SOURCE = "C:\msys64\home\Joey\.config\pfetch.conf"
+
+pfetch
