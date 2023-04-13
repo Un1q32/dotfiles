b4e620f3b3e7a2b3d1d48127fc1db344b5aeee0e
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-04-13 07:50:56 -0400

    always install everything noice needs

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "bash", "lua" },
+  ensure_installed = { "bash", "lua", "vim", "regex", "markdown", "markdown_inline" },
   sync_install = false,
   auto_install = true,
   highlight = {
