c95d885b1eaf9609f1a85185acebd318a2a728f0
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-04-17 13:15:46 -0400

    use lazy.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,46 +1,41 @@
-local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-        vim.cmd [[packadd packer.nvim]]
-        return true
-    end
-    return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
+local plugins = {
+    "github/copilot.vim",
+    "norcalli/nvim-colorizer.lua",
+    "akinsho/bufferline.nvim",
+    "OldWorldOrdr/nvim-base16",
+    "nvim-tree/nvim-tree.lua",
+    "nvim-tree/nvim-web-devicons",
+    "tpope/vim-commentary",
+    "mfussenegger/nvim-lint",
+    "sheerun/vim-polyglot",
+    "folke/noice.nvim",
+    "rcarriga/nvim-notify",
+    "MunifTanjim/nui.nvim",
+    "nvim-treesitter/nvim-treesitter",
 
-return require('packer').startup(function(use)
-    use 'github/copilot.vim'
-    use 'norcalli/nvim-colorizer.lua'
-    use 'akinsho/bufferline.nvim'
-    use 'wbthomason/packer.nvim'
-    use 'OldWorldOrdr/nvim-base16'
-    use 'nvim-tree/nvim-tree.lua'
-    use 'nvim-tree/nvim-web-devicons'
-    use 'tpope/vim-commentary'
-    use 'mfussenegger/nvim-lint'
-    use 'sheerun/vim-polyglot'
-    use 'folke/noice.nvim'
-    use 'rcarriga/nvim-notify'
-    use 'MunifTanjim/nui.nvim'
-    use 'nvim-treesitter/nvim-treesitter'
+    -- "hrsh7th/nvim-cmp",
+    -- "hrsh7th/cmp-nvim-lsp",
+    -- "L3MON4D3/LuaSnip",
+    -- "saadparwaiz1/cmp_luasnip",
+    -- "rafamadriz/friendly-snippets",
+    -- "williamboman/mason.nvim",
+    -- "williamboman/mason-lspconfig.nvim",
+    -- "neovim/nvim-lspconfig",
+}
 
-    -- use 'hrsh7th/nvim-cmp'
-    -- use 'hrsh7th/cmp-nvim-lsp'
-    -- use 'L3MON4D3/LuaSnip'
-    -- use 'saadparwaiz1/cmp_luasnip'
-    -- use 'rafamadriz/friendly-snippets'
+local opts = {}
 
-    -- use {
-    --   'williamboman/mason.nvim',
-    --   'williamboman/mason-lspconfig.nvim',
-    --   'neovim/nvim-lspconfig',
-    -- }
-
-    use 'sakhnik/nvim-gdb'
-    if packer_bootstrap then
-        require('packer').sync()
-    end
-end)
+require("lazy").setup(plugins, opts)
