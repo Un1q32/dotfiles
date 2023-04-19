0074131d61d0fe1e2d5c3e5e0291c236bb046240
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-04-19 11:55:55 -0400

    lint more

--- .config/nvim/lua/config/lint.lua
+++ .config/nvim/lua/config/lint.lua
@@ -5,7 +5,7 @@ require('lint').linters_by_ft = {
 
 vim.fn.setenv('SHELLCHECK_OPTS', '--exclude SC2148')
 
-vim.api.nvim_create_autocmd({ "InsertLeave", "BufEnter" }, {
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "CursorHold", "CursorHoldI", "InsertLeave", "WinEnter" }, {
     callback = function()
         require('lint').try_lint()
     end,
