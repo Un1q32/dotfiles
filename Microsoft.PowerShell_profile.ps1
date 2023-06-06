b4e5666c95dbd4ef19ddc72216f14fa2746f5a35
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-06-06 01:49:45 -0400

    change aliases

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,5 @@
-Set-Alias rm $HOME\scoop\shims\rm.exe
+Set-Alias rm rm.exe
 Set-Alias find $HOME\scoop\shims\find.exe
-Set-Alias convert $HOME\scoop\apps\imagemagick\current\convert.exe
 Set-Alias lg lazygit
 Set-Alias ls real_ls
 Set-Alias cat real_cat
@@ -13,6 +12,7 @@ function real_ls { busybox ls --color --group-directories-first -AA $args }
 function l { busybox ls --color --group-directories-first -AAlh $args }
 function real_cat { bat --theme=ansi -P $args }
 function ln { sudo New-Item -ItemType SymbolicLink -Path $args[1] -Target $args[0] }
+function convert { magick convert $args }
 function tree { tree.exe -aCI .git --dirsfirst $args }
 function server { ssh joey@192.168.1.232 -p2222 }
 function pcserver { ssh joey@192.168.1.233 }
