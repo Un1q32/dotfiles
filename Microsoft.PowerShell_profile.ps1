e7e1da8f9390a7ebefac04758d49141f025e750a
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-06-23 03:13:53 -0400

    fix

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -11,7 +11,7 @@ function .... { Set-Location "../../.." }
 function ..... { Set-Location "../../../.." }
 function real_ls { busybox ls --color --group-directories-first -AA $args }
 function l { busybox ls --color --group-directories-first -AAlh $args }
-function real_cat { bat --theme=ansi -P $args }
+function real_cat { bat -P $args }
 function ln { sudo New-Item -ItemType SymbolicLink -Path $args[1] -Target $args[0] }
 function convert { magick convert $args }
 function tree { tree.exe -aCI .git --dirsfirst $args }
