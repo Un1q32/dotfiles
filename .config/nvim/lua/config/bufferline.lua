3b75f916742ca296756630d756e1efb6e94321ef
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-07-14 22:07:45 -0400

    fix

--- .config/nvim/lua/config/bufferline.lua
+++ .config/nvim/lua/config/bufferline.lua
@@ -2,7 +2,6 @@ local bufferline = require('bufferline')
 
 bufferline.setup({
     options = {
-        style_preset = bufferline.style_preset.no_italic,
         separator_style = 'slant',
         hover = {
             enabled = true,
