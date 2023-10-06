ed7b6aa6681211baa2017c8505625be4d5ed67f8
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-10-06 01:36:18 -0400

    add diff, yaml, toml to nvim treesitter ensure installed

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup({
-  ensure_installed = { 'bash', 'lua', 'vim', 'regex', 'markdown', 'markdown_inline', 'vimdoc', 'c' },
+  ensure_installed = { 'bash', 'lua', 'vim', 'regex', 'markdown', 'markdown_inline', 'vimdoc', 'c', 'diff', 'yaml', 'toml' },
   sync_install = false,
   auto_install = true,
   highlight = { enable = true }
