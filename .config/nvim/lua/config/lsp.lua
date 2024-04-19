9e438ab8f96833c25edabdd3c87e4876137dd3b7
Un1q32 <joey.t.reinhart@gmail.com>
2024-04-19 00:51:33 -0400

    ensure installed maso

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,5 +1,10 @@
 local lsp = require("lspconfig")
 
+require("mason").setup()
+require("mason-lspconfig").setup {
+  ensure_installed = { "lua_ls", "clangd", "bashls" }
+}
+
 lsp.clangd.setup({})
 lsp.bashls.setup({})
 lsp.lua_ls.setup({
