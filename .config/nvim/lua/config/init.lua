143004688de6975b3ac0816ceaf39597832bd4a9
Un1q32 <joey.t.reinhart@gmail.com>
2024-04-24 01:53:10 -0400

    remove format-on-save, add bind instead

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -2,7 +2,6 @@ require('config.treesitter')
 if vim.fn.system('uname -o') ~= 'Android\n' then
   require('config.lsp')
 end
-require('config.format')
 
 require("nvim-autopairs").setup()
 require('Comment').setup()
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', { silent = true })
+vim.keymap.set('n', '<leader>f', ':lua vim.lsp.buf.format()<CR>', { silent = true })
 
 vim.opt.number = true
 vim.opt.showmode = false
