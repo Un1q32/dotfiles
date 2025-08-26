47a4e601c629251325d3dc3d8a92ff2a4b20b198
Un1q32 <joey.t.reinhart@gmail.com>
2025-08-26 15:10:48 -0400

    update powershell config

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,26 +1,12 @@
-Set-Alias rm rm.exe
-Set-Alias find $HOME\scoop\shims\find.exe
 Set-Alias lg lazygit
 Set-Alias ls real_ls
-Set-Alias cat real_cat
-Set-Alias sudo gsudo
-Set-Alias ar llvm-ar
-Set-Alias strings llvm-strings
-Set-Alias strip llvm-strip
-Set-Alias bash C:\Users\Joey\scoop\shims\bash.exe
 function .. { Set-Location ".." }
 function ... { Set-Location "../.." }
 function .... { Set-Location "../../.." }
 function ..... { Set-Location "../../../.." }
 function real_ls { busybox ls --color --group-directories-first -AA $args }
-function l { busybox ls --color --group-directories-first -AAlh $args }
-function real_cat { bat -P $args }
-function ln { sudo New-Item -ItemType SymbolicLink -Path $args[1] -Target $args[0] }
-function convert { magick convert $args }
-function tree { tree.exe -aCI .git --dirsfirst $args }
-function objdump { llvm-objdump -M intel $args }
+function l { real_ls -lh $args }
 
-Import-Module gsudoModule
 Import-Module PSReadLine
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadlineOption -EditMode vi
@@ -38,8 +24,5 @@ function prompt {
     Write-Host "$(promptpath) `$" -ForegroundColor Blue -NoNewLine
     return ' '
 }
-
-$env:CARGO_REGISTRIES_CRATES_IO_PROTOCOL = "sparse"
-$env:PF_SOURCE = "$HOME\.config\pfetch.conf"
 
 pfetch
