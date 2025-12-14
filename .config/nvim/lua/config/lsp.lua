253f96142708f16561093c843184e74245de02ef
Un1q32 <joey.t.reinhart@gmail.com>
2025-12-14 00:58:14 -0500

    disable semantic token parsing

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -3,6 +3,14 @@ require("mason-lspconfig").setup {
   ensure_installed = { "lua_ls", "clangd", "bashls" }
 }
 
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    client.server_capabilities.semanticTokensProvider = nil
+  end,
+})
+
 vim.lsp.enable('bashls')
 vim.lsp.enable('lua_ls')
 vim.lsp.config('lua_ls', {
