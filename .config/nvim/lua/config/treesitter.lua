47d3679a1322fcf7d6e89944c50938ad458a5c51
Un1q32 <joey.t.reinhart@gmail.com>
2025-12-25 21:57:50 -0800

    fix treesitter config

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -1,6 +1,1 @@
-require 'nvim-treesitter.configs'.setup({
-  ensure_installed = { 'bash', 'lua', 'vim', 'regex', 'markdown', 'markdown_inline', 'vimdoc', 'c', 'diff', 'yaml', 'toml' },
-  sync_install = false,
-  auto_install = true,
-  highlight = { enable = true }
-})
+require'nvim-treesitter'.install { 'bash', 'lua', 'vim', 'regex', 'markdown', 'markdown_inline', 'vimdoc', 'c', 'diff', 'yaml', 'toml' }
