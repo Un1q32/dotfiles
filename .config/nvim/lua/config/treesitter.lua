7b7f1d82cc49d5099592148eb29ce80a2050cc3b
Un1q32 <joey.t.reinhart@gmail.com>
2026-01-03 20:56:49 -0500

    fix

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -1,8 +1,12 @@
 local ts = require'nvim-treesitter'
-local filetypes = { 'bash', 'sh', 'lua', 'vim', 'regex', 'markdown', 'markdown_inline', 'vimdoc', 'c', 'diff', 'yaml', 'toml', 'make', 'groovy' }
+local filetypes = { 'bash', 'lua', 'vim', 'regex', 'markdown', 'markdown_inline', 'vimdoc', 'c', 'diff', 'yaml', 'toml', 'make', 'groovy' }
 
 ts.install(filetypes):wait(9999999)
 vim.api.nvim_create_autocmd('FileType', {
   pattern = filetypes,
+  callback = function() vim.treesitter.start() end,
+})
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'sh',
   callback = function() vim.treesitter.start() end,
 })
