7fa5c5473a4cbcddf3aa357a6f0b6c0783fdd44f
Un1q32 <joey.t.reinhart@gmail.com>
2026-01-04 10:36:29 -0500

    add gitcommit

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 local ts = require'nvim-treesitter'
-local filetypes = { 'bash', 'lua', 'vim', 'regex', 'markdown', 'markdown_inline', 'vimdoc', 'c', 'diff', 'yaml', 'toml', 'make', 'groovy', 'cmake' }
+local filetypes = { 'bash', 'lua', 'vim', 'regex', 'markdown', 'markdown_inline', 'vimdoc', 'c', 'diff', 'yaml', 'toml', 'make', 'groovy', 'cmake', 'gitcommit' }
 
 ts.install(filetypes):wait(9999999)
 vim.api.nvim_create_autocmd('FileType', {
